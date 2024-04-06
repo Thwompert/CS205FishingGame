289bfd931cd274d9537ac22da1b0524f90410194
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2024-04-06 13:50:05 +0800

    added some text to backstory page

--- app/src/main/java/com/example/cs205fishinggame/HelpActivity.java
+++ app/src/main/java/com/example/cs205fishinggame/HelpActivity.java
@@ -4,12 +4,29 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
 public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help); // Make sure this matches your XML layout file name
+        setContentView(R.layout.activity_help);
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        TextView backstoryTextView = findViewById(R.id.backstoryTextView);
+        String backstory = "In the year 2100, the vibrant city-state of Singapore, " +
+                "once a bustling metropolis, now lies beneath the waves. " +
+                "Climate change and rising sea levels have irreversibly transformed " +
+                "the landscape, submerging the once-thriving nation under the ocean's embrace. " +
+                "The iconic Marina Bay Sands and the majestic Merlion are now part of an underwater realm, " +
+                "creating a surreal fusion of the past and the marine world.";
+        backstoryTextView.setText(backstory);
     }
 
     // This method is called when the back button is clicked
